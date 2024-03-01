0cdeff7f299484021432e24aa9441ccd8cf5056f
Veronica Viscardi <veronica05viscardi@gmail.com>
2024-03-01 10:48:32 +0000

    ImageGrid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,24 @@
-import "./App.css";
-import { useState } from "react";
-import UserView from "./components/UserView";
-import AdminView from "./components/AdminView";
-import ImageGrid from "ImageGrid.js";
+import React, { useState } from 'react';
+import './App.css';
+import AdminView from './components/AdminView';
+import UserView from './components/UserView';
 
+function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const toggleView = () => {
+    setIsAdmin((prevIsAdmin) => !prevIsAdmin);
+  };
+
+  return (
+    <div className="App">
+      <h1>Secret Santa App</h1>
+      <button onClick={toggleView}>
+        Switch to {isAdmin ? 'User View' : 'Admin View'}
+      </button>
+      {isAdmin ? <AdminView /> : <UserView />}
+    </div>
+  );
+}
+
+export default App;
