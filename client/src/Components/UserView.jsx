e832d553e2fdc23fd97135d929b47b8f280ac8b6
Veronica Viscardi <veronica05viscardi@gmail.com>
2024-03-06 11:40:37 +0000

    Database updated

--- client/src/Components/UserView.jsx
+++ client/src/Components/UserView.jsx
@@ -1,4 +1,4 @@
-// UserView.jsx
+
 import React, { useState, useEffect } from 'react';
 import './UserView.css';
 
--- model/init_db.sql
+++ model/init_db.sql
@@ -11,23 +11,19 @@ SET foreign_key_checks = 1;
 --
 -- Create Tables
 --
-CREATE TABLE children (
-    id INT NOT NULL Auto_Increment,
-    childrenGender VARCHAR (10) not null,
-    childrenAge INTEGER,
-    PRIMARY KEY (id)
-);
+
 CREATE TABLE donations (
     id INT NOT NULL AUTO_INCREMENT, 
     item VARCHAR(100) NOT NULL, 
     itemPrice INTEGER NOT NULL,
     itemUrl VARCHAR(2500) NOT NULL,
     itemDescription VARCHAR(500) NOT NULL,
+    itemImg VARCHAR (2500) NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT into donations (item,itemPrice,itemUrl,itemDescription) VALUES
-('Handcream', 5, 'https://www.amazon.co.uk/Yardley-London-English-Nourishing-Cream/dp/B00UVR3JUU/ref=asc_df_B00UVR3JUU&mcid=4632b5b9678d3fe89b8bb2dfff8b98e8?tag=bingshoppinga-21&linkCode=df0&hvadid=80126967116319&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=&hvtargid=pla-4583726541596242&psc=1', 'Rose Scented Handcream'),
+INSERT into donations (item,itemPrice,itemUrl,itemDescription, itemImg) VALUES
+('Handcream', 5, 'https://www.amazon.co.uk/Yardley-London-English-Nourishing-Cream/dp/B00UVR3JUU/ref=asc_df_B00UVR3JUU&mcid=4632b5b9678d3fe89b8bb2dfff8b98e8?tag=bingshoppinga-21&linkCode=df0&hvadid=80126967116319&hvnetw=o&hvqmt=e&hvbmt=be&hvdev=c&hvlocint=&hvlocphy=&hvtargid=pla-4583726541596242&psc=1', 'Rose Scented Handcream', 'https://m.media-amazon.com/images/I/71zv0eKF3SL.__AC_SX300_SY300_QL70_ML2_.jpg'),
 ('Football', 10, 'https://www.amazon.co.uk/Nike-Pitch-Unisex-Green-Electric/dp/B01N0TSQ4H/ref=sr_1_5?dib=eyJ2IjoiMSJ9.l3u9--qNlMkA634ve5-ersQnb6Agz1w_Y_ZXV50U7fx89HL3DHuSLVMz3vaqPTvdfb77HrSZZ2tLin6gToWrSwcMLv5FAj9s83R4XuHgM4I7Abf64ooymsmB-q2f2xGEfqk2CAb1n9PQPZ9olOsOXKmY-Qo1EMc8L2eY2sm4RFJokUrrY2M-rsMS0fozA8yyVbLzdW8K6ZrPAvOywKe2v7pJXcd4rMJtfWaPmOuiT0zHaSO1XW9HgnkI0vaSEJGy7DAk0VqHtgsAgMJjyDj0bvRbIRjLXrWL5MNzuLnvp5w.ca3YqPIEllPYWaeqt8xZrCmL7ThUBND6EFay-ygjCaw&dib_tag=se&keywords=football&qid=1709206081&sr=8-5', 'Nike Pitch Football'),
 ('Kettlebell', 20, 'https://www.therange.co.uk/leisure/sport/fitness-equipment-and-accessories/home-gym/kettlebells/active-sport-kettle-bell/?msclkid=4131debbbbea1b34eb26bb5cafb0f051&utm_source=bing&utm_medium=cpc&utm_campaign=PLA||Low||All%20Products||New&utm_term=4575067903842612&utm_content=PLA||All%20Products#35797', 'Selection of Fitness Kettlebell'),
 ('Craft kit', 25, 'https://www.notonthehighstreet.com/craftiosity/product/paper-quilling-clock-craft-kit?attributionToken=jQHwjAoMCLStnK8GEJbitbgBEAEaJDY1ZWUyZmRjLTAwMDAtMmI1Ny1hM2M4LWY0MDMwNDNkODhmYyobR0ExLjIuMTI5MzkxNzcwNi4xNzA5NjQzNDI2MiTKyZIio4CXIriZoSKN96ciwvCeFcbL8xeOvp0V1LKdFZD3pyI6DmRlZmF1bHRfYnJvd3NlSAFoAQ&referredBy=plp','Choice of crafting kits'),
